d2802744722c2e298b8dcef34d524f4f536e3104
pmudry <pmudry@gmail.com>
2023-08-04 16:38:15 +0200

    Added option to copy result

--- build_tool/build_pandoc.sh
+++ build_tool/build_pandoc.sh
@@ -7,16 +7,20 @@ DIR="."
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":hn:" option; do
+while getopts ":hno:" option; do
    case $option in
       n) # Enter a file name
          DIR="$OPTARG"
          ;;
       h) # not yet implemented
-      echo "not yet implemented"
-      exit;;
-     \?) # Invalid option
-         echo "Error: Invalid option"
+         echo "not yet implemented"
+         exit
+         ;;
+      o) # Destination of PDF file        
+         DEST_PDF="$OPTARG"
+         ;;
+      \?) # Invalid option
+         echo "Error: invalid option"
          exit;;
    esac
 done
@@ -39,7 +43,11 @@ pushd "$DIR" || exit
 # We also pass the current script directory path for pandoc to find the template. We also require to pass the toolchain directory for including the image once. For this reason, we pass the location of the directory as a variable (which is not escaped by pandoc, and not as a metadata (which is the same as the values given in the YAML header of the .md))
 pandoc "${input}" -o "${output}" --pdf-engine=xelatex --from markdown+tex_math_dollars+raw_tex --template="$SCRIPT_DIR/isc_lab.tex" --listings -V colorlinks --number-sections --variable=TOOLCHAINPATH:"$SCRIPT_DIR" --metadata=DRAFT:false --metadata=GENERATORVERSION:"$VERSION" #--verbose
 
-# cp "${output}" ../pdf/
+if [ -n "$DEST_PDF" ]; then    
+   echo "Output generated in ${output} and copied to PDF directory ${DEST_PDF}"   
+   cp "${output}" "${DEST_PDF}"
+else 
+   echo "Output generated in ${output}"
+fi
 
-echo "Output generated in ${output} and copied to PDF directory"
 popd || exit
