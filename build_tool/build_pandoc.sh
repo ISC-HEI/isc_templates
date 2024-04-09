c8924f00e7791ce295adcd83b93b3846f68c3ae6
pmudry <pmudry@gmail.com>
2024-04-09 06:17:31 +0200

    Update for oral exams.

--- build_tool/build_pandoc.sh
+++ build_tool/build_pandoc.sh
@@ -2,18 +2,23 @@
 VERSION="1.0.1"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DIR="."
+TEMPLATE="isc_lab.tex"
 
 ############################################################
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":hno:" option; do
+while getopts ":ntho:" option; do
    case $option in
-      n) # Enter a file name
+      n) # Working directory
          DIR="$OPTARG"
          ;;
-      h) # not yet implemented
-         echo "not yet implemented"
+      t) # ORAL EXAM TEMPLATE
+         echo "- Using oral exam template"
+         TEMPLATE="isc_oral_exam.tex"
+         ;;
+      h) # not yet implemented         
+         echo "For building an oral exam using the specific template switch, -t"
          exit
          ;;
       o) # Destination of PDF file        
@@ -33,7 +38,7 @@ if [ -z "$input" ]; then
     exit 1
 fi
 
-echo "Compiling file '${input}'"
+echo "- Compiling file '${input}'"
 input=$(basename "$input")
 output=${input/'.md'/'.pdf'}
 
@@ -41,13 +46,13 @@ pushd "$DIR" || exit
 
 # Creating the document, using XeTex as engine
 # We also pass the current script directory path for pandoc to find the template. We also require to pass the toolchain directory for including the image once. For this reason, we pass the location of the directory as a variable (which is not escaped by pandoc, and not as a metadata (which is the same as the values given in the YAML header of the .md))
-pandoc "${input}" -o "${output}" --pdf-engine=xelatex --from markdown+tex_math_dollars+raw_tex --template="$SCRIPT_DIR/isc_lab.tex" --listings -V colorlinks --number-sections --variable=TOOLCHAINPATH:"$SCRIPT_DIR" --metadata=DRAFT:false --metadata=GENERATORVERSION:"$VERSION" #--verbose
+pandoc "${input}" -o "${output}" --pdf-engine=xelatex --from markdown+tex_math_dollars+raw_tex --template="$SCRIPT_DIR/$TEMPLATE" --listings -V colorlinks --number-sections --variable=TOOLCHAINPATH:"$SCRIPT_DIR" --metadata=DRAFT:false --metadata=GENERATORVERSION:"$VERSION" #--verbose
 
 if [ -n "$DEST_PDF" ]; then    
-   echo "Output generated in ${output} and copied to PDF directory ${DEST_PDF}"   
+   echo "- Output generated in ${output} and copied to PDF directory ${DEST_PDF}"   
    cp "${output}" "${DEST_PDF}"
 else 
-   echo "Output generated in ${output}"
+   echo "- Output generated in ${output}"
 fi
 
 popd || exit
