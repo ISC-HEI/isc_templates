0b05b4f0e0d3d1f15d3326d8b7b8f538e164356c
P.-A <pmudry@gmail.com.com>
2024-05-11 11:27:12 +0200

    Polishing math engine setup

--- build_tool/build_html.sh
+++ build_tool/build_html.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
-VERSION="1.0.5"
+VERSION="1.0.6"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DIR="."
+MATH_ENGINE="katex"
 
 ############################################################
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":hno:" option; do
-   case $option in
-      n) # Enter a file name
+while getopts ":m:o:h:n:" option; do
+   case ${option} in
+      n ) # Enter a file name
          DIR="$OPTARG"
          ;;
-      h) # not yet implemented
+      h ) # not yet implemented
          echo "not yet implemented"
-         exit
+         exit 1
          ;;
-      o) # Destination of PDF file        
+      m ) # Math engine used
+         MATH_ENGINE=$OPTARG
+         echo "Using math engine $MATH_ENGINE"
+         ;;
+      o ) # Destination of PDF file        
          DEST_PDF="$OPTARG"
          ;;
-      \?) # Invalid option
-         echo "Error: invalid option"
-         exit;;
+      \? ) # Invalid option
+         echo "Error: invalid option -${OPTARG}"
+         exit 1;;
    esac
 done
+
+shift $((OPTIND -1))
 
 file=("$DIR"/*.md)
 input=${file[0]}
@@ -40,9 +47,9 @@ output=${input/'.md'/'.html'}
 pushd "$DIR" || exit
 
 # --css="$SCRIPT_DIR/html_templates/sakura.css"
-# options for maths is mathjax, mathml, katex
+# options for maths is mathjax, mathml, katex, webtex
 # mathml is fast but ugly. katex is fine
-pandoc "${input}" -o "${output}" --from markdown+tex_math_dollars+raw_tex+emoji --webtex --data-dir="$SCRIPT_DIR/html_templates" --template="GitHub2.html5" --toc --toc-depth=2 --embed-resources --standalone 
+pandoc "${input}" -o "${output}" --from markdown+tex_math_dollars+raw_tex+emoji --"${MATH_ENGINE}" --data-dir="$SCRIPT_DIR/html_templates" --template="GitHub2.html5" --toc --toc-depth=2 --embed-resources --standalone 
 
 if [ -n "$DEST_PDF" ]; then    
    echo "Output generated in ${output} and copied to directory ${DEST_PDF}"   
