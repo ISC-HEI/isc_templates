057d1b6091d7ffc07354ae7855b4b08396424a17
Eteroclia <maechlerl@gmail.com>
2024-08-06 10:17:04 +0200

    added mobile capabilities and other fixes

--- samples/sample_lab_html/build_html.sh
+++ samples/sample_lab_html/build_html.sh
@@ -1,3 +1,3 @@
 if [ -z $ISC_TOOLCHAIN ]; then echo "ISC_TOOLCHAIN variable not set up. It must point to a valid ISC template repository. Exiting."; exit -1; fi
 
-$ISC_TOOLCHAIN/build_html.sh -m webtex -o html/
+$ISC_TOOLCHAIN/build_html.sh -m mathml -o html/
---
+++ samples/sample_lab_html/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ISC_TOOLCHAIN=~/Desktop/HES/stage_ISC/isc_templates/build_tool
+echo "Exported path !"
+
+
+#run with source setup.sh
